b37d8d0c0505189967646c7d8a270730ed6f3836
Ankush Sood <a.sood@thisisdmg.com>
2023-01-05 18:45:15 +0530

    font updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,9 @@
 
         <!--Google fonts links-->
         <!-- <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet"> -->
-        <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet'>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet"> 
         <link rel="stylesheet" href="assets/css/bootstrap.min.css">
 
 
