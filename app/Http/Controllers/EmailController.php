383c8fecdca5abe834882b1265a7d15ec4a5e7a5
Ankush Sood <a.sood@thisisdmg.com>
2023-01-08 11:13:39 +0530

    working on email functionality

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\EmailRequest;
 use App\Models\PreLaunchEmail;
+use Mail;
+use App\Mail\VerifyEmail;
 
 class EmailController extends Controller
 {
@@ -14,11 +16,12 @@ class EmailController extends Controller
     public function storeEmail(EmailRequest $request) 
     {
         $validateEmail = $request->validated();
-        PreLaunchEmail::create([
+        $newUser = PreLaunchEmail::create([
             'email' => $request->email,
             'is_verified' => false,
             'token' => md5(rand())
         ]);
+        Mail::to('aks21117@gmail.com')->send(new VerifyEmail());
         return redirect()->back()->with(['status' => 'Email registered. Please verify it.']);
     }
 }
---
+++ resources/views/mails/verify_email.blade.php
@@ -0,0 +1,45 @@
+Hello,
+
+Thank you for signing up for our social media content scheduling tool!
+
+To verify your email address and complete the sign-up process, please click on the following link:
+
+[Insert verification link]
+
+Once you have verified your email, you will be able to join the waitlist to try the mateauto social media content management system.
+
+Best,
+Ankush
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Our Company</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<h1 class="text-center">Welcome to Our Company</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<p>Dear [Name],</p>
+				<p>We are so glad to have you join our team! We are confident that you will thrive in your new role and contribute greatly to the success of our company.</p>
+				<p>To get started, please log in to your account using the following information:</p>
+				<ul>
+					<li>Username: [Username]</li>
+					<li>Password: [Password]</li>
+				</ul>
+				<p>Please do not hesitate to reach out to your supervisor or HR if you have any questions or concerns.</p>
+				<p>Welcome aboard!</p>
+				<p>Sincerely,</p>
+				<p>[Company Name]</p>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
