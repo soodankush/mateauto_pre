f70c93a51df7ffdbb58c93e8cc3bb48e3bbb37cf
Ankush Sood <a.sood@thisisdmg.com>
2023-02-18 01:37:11 +0530

    login and register api completed

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
+use Hash;
 
 class AuthController extends Controller
 {
@@ -39,5 +40,46 @@ class AuthController extends Controller
         ];
 
         return response()->json($response, 200);
+    }
+
+    public function login(Request $request) 
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        if($validator->fails()) {
+            $response = [
+                'success' => false,
+                'message' => $validator->errors()
+            ];
+            return response()->json($response, 400);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        if(!$user) {
+            $response = [
+                'success' => false,
+                'message' => 'Invalid credentials'
+            ];
+            return response()->json($response, 400);
+        }
+
+        if(!Hash::check($request->password, $user->password))
+        {
+            $response = [
+                'success' => false,
+                'message' => 'Invalid credentials'
+            ];
+            return response()->json($response, 400);   
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'User Logged In Successfully',
+            'token' => $user->createToken("MyApp")->plainTextToken
+        ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/register', 'App\Http\Controllers\API\AuthController@register');
+Route::post('/login', 'App\Http\Controllers\API\AuthController@login');
